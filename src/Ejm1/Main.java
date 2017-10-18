e2c31b8baf9ba2d7178448430d1fd0cbb7afcfef
adragonrc <adragonrc@gmail.com>
2017-10-17 22:24:38 -0500

    Correccion de errores e identacion

--- src/Ejm1/Main.java
+++ src/Ejm1/Main.java
@@ -38,12 +38,23 @@ public class Main {
     }
   
     public static void main(String[] args) throws IOException{
-        String titular ;
-        float saldo=0;
+        String titular, res ;
+        float saldo;
+        Cuenta cuenta1;
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        
         System.out.println("Ingrese su nombre");
         titular = br.readLine();
-        Cuenta cuenta1= new Cuenta(titular,saldo );
+        System.out.println("Desea agregar un monto inicial? [S/n]");
+        res = br.readLine();
+        if("s".equals(res) || "S".equals(res)){
+            System.out.print("Monto que desea agregar: ");
+            saldo=Float.parseFloat(br.readLine());
+            cuenta1 = new Cuenta(titular,saldo); 
+        }
+        else{
+            cuenta1 = new Cuenta(titular);
+        }
         System.out.println("bienvenido " + titular);
         int flag1;
         flag1 = 0;
@@ -51,60 +62,54 @@ public class Main {
         flag2 = 0;
         int f;
         f = menu(cuenta1.getsaldo());
-        int flag3=0;
-    while(flag3==0){
-        if(f==1){
-            while(flag1==0&&f==1){
-            String r;
-           
-            System.out.print("Ingrese el monto que desea retirar: ");
-            saldo=Float.parseFloat(br.readLine());
-            if(cuenta1.getsaldo()-saldo<0)
-            {
-                System.out.println("Lo sentimos no cuenta con dinero suficiente");
-            }
-            else{
-             
-                saldo=cuenta1.getsaldo()-saldo;
-                cuenta1.setsaldo(saldo);
-                System.out.println("A modificado el saldo de su cuenta a:"+cuenta1.getsaldo());
-                System.out.println("Gracias por su operacion");
-                System.out.println("Desea volver al menu? [S/n]");
-                r=br.readLine();
-                if("s".equals(r)||"S".equals(r)){
-                     f=menu(cuenta1.getsaldo());
-                } 
-            else {
-                    flag1=1;
-                    flag3=2;
+        int flag3 = 0;
+        while(flag3 == 0){
+            if(f == 1){
+                while(flag1==0 && f==1){
+                    String r;
+                    System.out.print("Ingrese el monto que desea retirar: ");
+                    saldo=Float.parseFloat(br.readLine());
+                    if(cuenta1.getsaldo()-saldo<0){
+                        System.out.println("Lo sentimos no cuenta con dinero suficiente");
+                    }
+                    else{
+                        saldo=cuenta1.getsaldo()-saldo;
+                        cuenta1.setsaldo(saldo);
+                        System.out.println("A modificado el saldo de su cuenta a:"+cuenta1.getsaldo());
+                        System.out.println("Gracias por su operacion");
+                        System.out.println("Desea volver al menu? [S/n]");
+                        r = br.readLine();
+                        if("s".equals(r)||"S".equals(r)){
+                            f = menu(cuenta1.getsaldo());
+                        } 
+                        else {
+                            flag1 = 1;
+                            flag3 = 2;
+                        }
+                    }
                 }
             }
+            if(f == 2){
+                while(flag2==0 && f==2){
+                    String r;
+                    System.out.print("ingrese el monto que desa agregar a su cuenta: ");
+                    saldo = Float.parseFloat(br.readLine());
+                    saldo = cuenta1.getsaldo()+saldo;
+                    cuenta1.setsaldo(saldo);
+                    System.out.println("A modificado el saldo de su cuenta a:"+cuenta1.getsaldo());
+                    System.out.println("Gracias por su operacion");
+                    System.out.println("Desea volver al menu? [S/n]");
+                    r = br.readLine();
+                    if("s".equals(r) || "S".equals(r)){
+                        f = menu(cuenta1.getsaldo());
+                    } 
+                    else{
+                        flag2 = 1;
+                        flag3 = 3;
+                    }
+                }
             }
         }
-        if(f==2){
-        while(flag2==0&&f==2){
-            String r;
-           
-            System.out.print("ingrese el monto que desa agregar a su cuenta: ");
-            saldo=Float.parseFloat(br.readLine());
-            saldo=cuenta1.getsaldo()+saldo;
-            cuenta1.setsaldo(saldo);
-            System.out.println("A modificado el saldo de su cuenta a:"+cuenta1.getsaldo());
-            System.out.println("Gracias por su operacion");
-            System.out.println("Desea volver al menu? [S/n]");
-            
-            r=br.readLine();
-            if("s".equals(r)||"S".equals(r)){
-                f=menu(cuenta1.getsaldo());
-               
-            } 
-            else {flag2=1;
-                flag3=3;
-            }
-            
-        }
-        }
-    }
     }
 }
 
